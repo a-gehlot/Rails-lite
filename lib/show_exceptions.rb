f3c7b61b92ed50ccea182d12f5f6f031f670be38
AGehlot <andrew.gehlot@gmail.com>
2021-03-04 13:25:38 -0500

    exceptions rendering

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,26 @@
 require 'erb'
 
 class ShowExceptions
+
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    template = ERB.new((File.read('lib/rescue.html.erb')))
+    ['500', {'Content-type' => 'text/html'}, template.result(binding)]
   end
 
 end
