de5f4c166ac42e270d10db17d288c2ce1f68d329
AGehlot <andrew.gehlot@gmail.com>
2021-03-05 13:10:56 -0500

    more robust static assets

--- lib/static.rb
+++ lib/static.rb
@@ -1,20 +1,38 @@
 class Static
   attr_reader :app
+  
+  MIME_TYPES = {
+    '.txt' => 'text/plain',
+    '.jpg' => 'image/jpeg',
+    '.zip' => 'application/zip'
+    }
+
+  PUBLIC_REGEX = /\/public\/(.+)/
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
-    @path = env["PATH_INFO"]
-    if @path =~ /\/public\/.*/ && File.exist?(".#{@path}")
-      req = Rack::Request.new(env)
+    req = Rack::Request.new(env)
+
+    path = req.path
+
+    if path =~ PUBLIC_REGEX
       res = Rack::Response.new
-      res['Content-Type'] = 'text/plain'
-      template = File.read(".#{@path}")
+      match_regex = PUBLIC_REGEX.match(path)
+      file_name = "public/#{match_regex[1]}"
+
+      unless File.exist?(file_name)
+        res.status = 404
+        return res.finish
+      end
+
+      template = File.read(file_name)
+
       res.write(template)
+      res['Content-Type'] = File.extname(file_name)
       res.finish
-    else
-      [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
   end
 end
