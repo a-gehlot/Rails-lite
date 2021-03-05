45a25c16c633d74a63aa6aea5c2d853fbb59e3bf
AGehlot <andrew.gehlot@gmail.com>
2021-03-05 01:17:36 -0500

    static server methods

--- lib/static.rb
+++ lib/static.rb
@@ -1,7 +1,20 @@
 class Static
+  attr_reader :app
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    @path = env["PATH_INFO"]
+    if @path =~ /\/public\/.*/ && File.exist?(".#{@path}")
+      req = Rack::Request.new(env)
+      res = Rack::Response.new
+      res['Content-Type'] = 'text/plain'
+      template = File.read(".#{@path}")
+      res.write(template)
+      res.finish
+    else
+      [404, {"Content-Type" => "text/html"}, ["Not Found"]]
+    end
   end
 end
